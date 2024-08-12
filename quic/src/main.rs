5ec997d17d2bb72d32e20f6f01260b6b70b1958f
Ayodeji Akinola <akinayodeji4all@gmail.com>
2024-08-12 13:14:31 +0100

    quic example

---
+++ quic/src/main.rs
@@ -0,0 +1,158 @@
+use std::error::Error;
+use std::fmt::Debug;
+use std::net::{IpAddr, SocketAddr};
+use std::sync::Arc;
+use std::time::Duration;
+use anyhow::{Context, Result};
+use async_trait::async_trait;
+use clap::Parser;
+use gossipod::{DefaultBroadcastQueue, DefaultMetadata};
+use gossipod::{config::{GossipodConfigBuilder, NetworkType}, DispatchEventHandler, Gossipod, Node, NodeMetadata};
+use log::*;
+use tokio::sync::mpsc;
+use tokio::time;
+use quic::QuicTransport;
+mod quic;
+
+const NODE_NAME: &str = "QUINN_NODE_1";
+const BIND_PORT: u16 = 7948;
+
+struct SwimNode {
+    gossipod: Arc<Gossipod>,
+    receiver: mpsc::Receiver<Vec<u8>>,
+}
+
+struct EventHandler{
+    sender: mpsc::Sender<Vec<u8>>,
+}
+
+impl EventHandler {
+    fn new(sender: mpsc::Sender<Vec<u8>>) -> Self{
+        Self { sender }
+    }
+}
+
+#[async_trait]
+impl<M: NodeMetadata> DispatchEventHandler<M> for EventHandler {
+    async fn notify_dead(&self, node: &Node<M>) -> Result<(), Box<dyn Error + Send + Sync>>  {
+        info!("Node {} detected as dead", node.name);
+        Ok(())
+    }
+
+    async fn notify_leave(&self, node: &Node<M>) -> Result<(), Box<dyn Error + Send + Sync>>  {
+        info!("Node {} is leaving the cluster", node.name);
+        Ok(())
+    }
+
+    async fn notify_join(&self, node: &Node<M>) -> Result<(), Box<dyn Error + Send + Sync>>  {
+        info!("Node {} has joined the cluster", node.name);
+        Ok(())
+    }
+
+    async fn notify_message(&self, from: SocketAddr, message: Vec<u8>) -> Result<(), Box<dyn Error + Send + Sync>> {
+        info!("Received message from {}: {:?}", from, message);
+        self.sender.send(message).await?;
+        Ok(())
+    }
+}
+
+impl SwimNode {
+    async fn new(args: &Args) -> Result<Self> {
+        let config = GossipodConfigBuilder::new()
+            .name(&args.name)
+            .port(args.port)
+            .addr(args.ip.parse::<IpAddr>().expect("Invalid IP address"))
+            .probing_interval(Duration::from_secs(3))
+            .ack_timeout(Duration::from_millis(1_500))
+            .indirect_ack_timeout(Duration::from_secs(1))
+            .suspicious_timeout(Duration::from_secs(5))
+            .network_type(NetworkType::Local)
+            .build()
+            .await?;
+
+        let (sender, receiver) = mpsc::channel(1000);
+        let dispatch_event_handler = Arc::new(EventHandler::new(sender));
+    
+        let broadcast_queue =  Arc::new(DefaultBroadcastQueue::new(1));
+        let addr = SocketAddr::new(config.ip_addr(), args.port);
+        let quinn_transport = Arc::new(QuicTransport::new(addr).await?);
+
+        let gossipod = Gossipod::with_custom(config.clone(), DefaultMetadata::default(), broadcast_queue, quinn_transport, Some(dispatch_event_handler))
+        .await
+        .context("Failed to initialize Gossipod with custom metadata")?;
+        
+        Ok(SwimNode {
+            gossipod: Arc::new(gossipod),
+            receiver,
+        })
+    }
+    async fn start(&self) -> Result<()> {
+        let gossipod_clone = self.gossipod.clone();
+        tokio::spawn(async move {
+            if let Err(e) = gossipod_clone.start().await {
+                error!("[ERR] Error starting Gossipod: {:?}", e);
+            }
+        });
+
+        while !self.gossipod.is_running().await {
+            time::sleep(Duration::from_millis(100)).await;
+        }
+
+        let local_node = self.gossipod.get_local_node().await?;
+        info!("Local node: {}:{}", local_node.ip_addr, local_node.port);
+
+        Ok(())
+    }
+
+    async fn run(&self) -> Result<()> {
+
+        loop {
+            tokio::select! {
+                _ = tokio::signal::ctrl_c() => {
+                    info!("Signal received, stopping Gossipod...");
+                    self.gossipod.stop().await?;
+                    return Ok(());
+                }
+            }
+        }
+    }
+
+}
+
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    #[arg(long, default_value = NODE_NAME)]
+    name: String,
+
+    #[arg(long, default_value_t = BIND_PORT)]
+    port: u16,
+
+    #[arg(long, default_value = "127.0.0.1")]
+    ip: String,
+
+    #[arg(long)]
+    join_addr: Option<String>,
+}
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    let args = Args::parse();
+    env_logger::Builder::new()
+    .filter_level(::log::LevelFilter::Info) 
+    .filter_level(::log::LevelFilter::Debug)
+    .init();
+
+    let node = SwimNode::new(&args).await?;
+    node.start().await?;
+
+    if let Some(join_addr) = args.join_addr {
+        info!("Joining cluster via {}", join_addr);
+        node.gossipod.join(join_addr.parse()?).await?;
+    }
+
+    node.run().await?;
+
+    info!("Node stopped. Goodbye!");
+    Ok(())
+}
---
+++ quic/src/quic.rs
@@ -0,0 +1,213 @@
+use std::sync::Arc;
+use std::time::Duration;
+use std::collections::HashMap;
+use anyhow::{Result, anyhow};
+use gossipod::{Datagram, DatagramTransport};
+use quinn::{Endpoint, Connection, ClientConfig, ServerConfig, ConnectionError};
+use tokio::sync::{broadcast, Mutex, RwLock};
+use async_trait::async_trait;
+use std::net::SocketAddr;
+use log::*;
+use bytes::Bytes;
+
+
+pub struct QuicTransport {
+    endpoint: Endpoint,
+    datagram_tx: broadcast::Sender<Datagram>,
+    conns: Arc<RwLock<HashMap<SocketAddr, Arc<Mutex<Option<Connection>>>>>>,
+    shutdown_signal: broadcast::Sender<()>,
+}
+
+impl QuicTransport {
+    pub async fn new(bind_addr: SocketAddr) -> Result<Self> {
+        let (server_config, client_config) = build_quinn_configs().await?;
+        let mut endpoint = Endpoint::server(server_config, bind_addr)?;
+        endpoint.set_default_client_config(client_config);
+
+        let (datagram_tx, _) = broadcast::channel(10000);
+        let (shutdown_signal, _) = broadcast::channel(1);
+
+        let transport = Self {
+            endpoint,
+            datagram_tx,
+            conns: Arc::new(RwLock::new(HashMap::new())),
+            shutdown_signal,
+        };
+
+        transport.spawn_connection_listener();
+        Ok(transport)
+    }
+
+    fn spawn_connection_listener(&self) {
+        let endpoint = self.endpoint.clone();
+        let datagram_tx = self.datagram_tx.clone();
+        let conns = self.conns.clone();
+        let mut shutdown_rx = self.shutdown_signal.subscribe();
+
+        tokio::spawn(async move {
+            loop {
+                tokio::select! {
+                    Some(conn) = endpoint.accept() => {
+                        let remote_addr = conn.remote_address();
+                        info!("New QUIC connection from {}", remote_addr);
+                        
+                        let connection = match conn.await {
+                            Ok(c) => c,
+                            Err(e) => {
+                                error!("Failed to accept connection: {}", e);
+                                continue;
+                            }
+                        };
+                        
+                        let datagram_tx = datagram_tx.clone();
+                        let conns = conns.clone();
+
+                        tokio::spawn(async move {
+                            handle_connection(connection, remote_addr, datagram_tx, conns).await;
+                        });
+                    }
+                    _ = shutdown_rx.recv() => {
+                        info!("QUIC listener received shutdown signal");
+                        break;
+                    }
+                }
+            }
+            info!("QUIC listener shut down");
+        });
+    }
+
+    async fn connect(&self, addr: SocketAddr) -> Result<Connection, ConnectionError> {
+        let conn_lock = self.get_conn_lock(addr).await;
+        let mut lock = conn_lock.lock().await;
+
+        if let Some(conn) = lock.as_ref() {
+            if conn.close_reason().is_none(){
+                return Ok(conn.clone());
+            }
+        }
+
+        *lock = None;
+
+        let conn = self.measured_connect(addr, addr.ip().to_string()).await?;
+        *lock = Some(conn.clone());
+        Ok(conn)
+    }
+
+    async fn get_conn_lock(&self, addr: SocketAddr) -> Arc<Mutex<Option<Connection>>> {
+        let mut w = self.conns.write().await;
+        w.entry(addr).or_default().clone()
+    }
+
+    async fn measured_connect(&self, addr: SocketAddr, server_name: String) -> Result<Connection, ConnectionError> {
+        let start = std::time::Instant::now();
+        if let Ok(conn) = self.endpoint.connect(addr, &server_name).map_err(|e| anyhow!(e)) {
+            let duration = start.elapsed();
+            debug!("Connection to {} established in {:?}", addr, duration);
+            return Ok(conn.await?);
+        }
+        Err(ConnectionError::TimedOut)
+    }
+}
+
+async fn handle_connection(
+    connection: Connection,
+    remote_addr: SocketAddr,
+    datagram_tx: broadcast::Sender<Datagram>,
+    conns: Arc<RwLock<HashMap<SocketAddr, Arc<Mutex<Option<Connection>>>>>>,
+) {
+    debug!("New connection established with {}", remote_addr);
+    loop {
+        match connection.read_datagram().await {
+            Ok(bytes) => {
+                if bytes == Bytes::from_static(b"keep-alive") {
+                    debug!("Received keep-alive from {}", remote_addr);
+                    continue;
+                }
+                
+                debug!("Received datagram from {}: {} bytes", remote_addr, bytes.len());
+                let datagram = Datagram {
+                    remote_addr,
+                    data: bytes.to_vec(),
+                };
+                if datagram_tx.send(datagram).is_err() {
+                    error!("All QUIC receivers have been dropped");
+                    break;
+                }
+            },
+            Err(ConnectionError::ApplicationClosed(error)) => {
+                warn!("Connection closed by application for {}: {}", remote_addr, error);
+                warn!("Connection stats: {:?}", connection.stats());
+                break;
+            },
+            Err(ConnectionError::ConnectionClosed(error)) => {
+                warn!("Connection closed for {}: {:?}", remote_addr, error);
+                warn!("Connection stats: {:?}", connection.stats());
+                break;
+            },
+            Err(e) => {
+                error!("Error reading datagram from {}: {}", remote_addr, e);
+                break
+            }
+        }
+    }
+
+    // Remove the closed connection from the list
+    let mut conns = conns.write().await;
+    conns.remove(&remote_addr);
+    drop(conns);
+
+    info!("Connection handler for {} finished", remote_addr);
+}
+
+#[async_trait]
+impl DatagramTransport for QuicTransport {
+    fn incoming(&self) -> broadcast::Receiver<Datagram> {
+        self.datagram_tx.subscribe()
+    }
+
+    async fn send_to(&self, target: SocketAddr, data: &[u8]) -> Result<()> {
+        let conn = self.connect(target).await?;
+        let bytes = Bytes::copy_from_slice(data);
+        conn.send_datagram(bytes)?;
+        Ok(())
+    }
+
+    fn local_addr(&self) -> Result<SocketAddr> {
+        Ok(self.endpoint.local_addr()?)
+    }
+
+    async fn shutdown(&self) -> Result<()> {
+        self.shutdown_signal.send(())
+            .map_err(|_| anyhow!("Failed to send shutdown signal"))?;
+        
+        let conns = self.conns.read().await;
+        for conn_mutex in conns.values() {
+            if let Some(conn) = conn_mutex.lock().await.as_ref() {
+                conn.close(0u32.into(), b"Server shutting down");
+            }
+        }
+        
+        self.endpoint.wait_idle().await;
+        Ok(())
+    }
+}
+
+async fn build_quinn_configs() -> Result<(ServerConfig, ClientConfig)> {
+    let mut server_config = quinn_plaintext::server_config();
+    let client_config = quinn_plaintext::client_config();
+
+    let mut transport_config = quinn::TransportConfig::default();
+    transport_config.max_idle_timeout(Some(
+        Duration::from_secs(120).try_into().unwrap(),
+    ));
+    transport_config.keep_alive_interval(Some(Duration::from_secs(15)));
+    transport_config.max_concurrent_bidi_streams(32u32.into());
+    transport_config.max_concurrent_uni_streams(256u32.into());
+
+    let transport_config = Arc::new(transport_config);
+    server_config.transport_config(transport_config.clone());
+
+    info!("QUIC transport config: {:?}", transport_config);
+
+    Ok((server_config, client_config))
+}
